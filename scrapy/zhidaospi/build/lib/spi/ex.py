ebd0921b139b4b3310ec79600f8e566d516746d1
yunyuyuan <yunyuyuan@users.noreply.github.com>
2019-01-08 22:33:40 +0800

    none

--- scrapy/zhidaospi/build/lib/spi/ex.py
+++
@@ -1,4 +0,0 @@
-import os
-
-sql = 'curl http://10.156.10.81:6800/cancel.json -d project=zhidao_spider -d job=3c2cd75805ab11e9bf3b6045cb1c53b6'
-os.system(sql)
--- scrapy/zhidaospi/build/lib/spi/stop.py
+++
@@ -1,4 +0,0 @@
-import os
-
-sql = 'curl http://127.0.0.1:6800/cancel.json -d project=zhidao_spider -d job=3f71ca7e05b311e9beca9c5c8e259bb2'
-os.system(sql)
